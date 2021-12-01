d767538669a1bd6f32286cc89a583844817dda88
tyler.allen <tyler.allen@ufl.edu>
2021-11-30 21:49:38 -0500

    Removed my code from prefab RobotFreeAnim file. Restore prefab file to original state. Place my code in RobotRotation file.

---
+++ Assets/Demo/Scripts/RobotRotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotRotation : MonoBehaviour {
+
+	Vector3 rot = Vector3.zero;
+
+	// Use this for initialization
+	void Awake()
+	{
+		gameObject.transform.eulerAngles = rot;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 upAxis = Vector3.zero;
+		Vector3 lookAt = Camera.current.transform.position;
+
+		upAxis.y = 1;
+		lookAt.y = transform.position.y;
+
+		transform.LookAt(lookAt, upAxis);
+	}
+
+}
--- Assets/Third Party/RobotSphere/Assets/Scripts/RobotFreeAnim.cs
+++ Assets/Third Party/RobotSphere/Assets/Scripts/RobotFreeAnim.cs
@@ -18,47 +18,8 @@ public class RobotFreeAnim : MonoBehaviour {
 	// Update is called once per frame
 	void Update()
 	{
-		//CheckKey();
-		//gameObject.transform.eulerAngles = rot;
-
-		//Get ARCamera Angle
-		//Quaternion targetAngle = new Quaternion(Camera.main.transform.rotation.x, Camera.main.transform.rotation.y, Camera.main.transform.rotation.z, Camera.main.transform.rotation.w);
-
-		////Set object to ARCamera Angle, lerp the angle
-		//this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetAngle, Time.deltaTime * 5f);
-
-		//this.transform.LookAt(Camera.main.transform);
-
-		//      var flatVectorToTarget = this.transform.position - Camera.current.transform.forward;
-		//      flatVectorToTarget.y = 0.0f;
-		//var newRotation = Quaternion.LookRotation(flatVectorToTarget);
-		//      transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.fixedDeltaTime * 8);
-
-		//Vector3 targetPosition = new Vector3(Camera.main.transform.position.x, transform.position.y, Camera.main.transform.position.z);
-		//transform.LookAt(targetPosition);
-
-		//transform.rotation = Quaternion.LookRotation(Camera.main.transform.position - transform.position, Camera.main.transform.up);
-
-		//var cameraForward = Camera.current.transform.forward;
-		//var cameraBearing = new Vector3(cameraForward.x, 0, -cameraForward.z).normalized;
-		//transform.rotation = Quaternion.LookRotation(cameraBearing);
-
-		//var originalRotation = transform.rotation;
-		//transform.LookAt(Camera.current.transform);
-		//var newRotation = transform.rotation;
-
-		//var finalRotation = new Quaternion(originalRotation.x, newRotation.y, originalRotation.z, originalRotation.w);
-
-		//transform.rotation = Quaternion.Slerp(transform.rotation, finalRotation, Time.fixedDeltaTime * 8);
-
-		Vector3 upAxis = Vector3.zero;
-		Vector3 lookAt = Camera.current.transform.position;
-
-		upAxis.y = 1;
-		lookAt.y = transform.position.y;
-
-		transform.LookAt(lookAt, upAxis);
-
+		CheckKey();
+		gameObject.transform.eulerAngles = rot;
 	}
 
 	void CheckKey()
