71db93da785b5db17a1166060d3bdb46813eec2b
tyler.allen <tyler.allen@ufl.edu>
2021-11-30 21:33:23 -0500

    Create script to handle displaying robot text

---
+++ Assets/Demo/Scripts/RobotText.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using UnityEngine;
+
+public class RobotText : MonoBehaviour
+{
+    [SerializeField]
+    GameObject FirstFloatingText;
+
+    [SerializeField]
+    GameObject SecondFloatingText;
+
+    [SerializeField]
+    GameObject ThirdFloatingText;
+
+    [SerializeField]
+    GameObject FourthFloatingText;
+
+    [SerializeField]
+    GameObject Book;
+
+    void Awake()
+    {
+        FirstFloatingText.SetActive(false);
+        SecondFloatingText.SetActive(false);
+        ThirdFloatingText.SetActive(false);
+        FourthFloatingText.SetActive(false);
+        Book.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(InitializeText());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator InitializeText()
+    {
+        yield return new WaitForSeconds(5);
+
+        FirstFloatingText.SetActive(true);
+
+        yield return new WaitForSeconds(10);
+
+        FirstFloatingText.SetActive(false);
+        SecondFloatingText.SetActive(true);
+
+        yield return new WaitForSeconds(10);
+
+        SecondFloatingText.SetActive(false);
+        ThirdFloatingText.SetActive(true);
+
+        yield return new WaitForSeconds(10);
+        
+        ThirdFloatingText.SetActive(false);
+        FourthFloatingText.SetActive(true);
+
+        Book.SetActive(true);
+    }
+}
