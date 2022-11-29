b2963ea124710452a44df4a742e7049842d88e50
leodebug <leonel.cruz@ufl.edu>
2022-11-28 21:23:08 -0500

    Updated question panel
    The prompt for the floating panel has been updated with a thermodynamics problem. Indicators have been added in the form of sprites next to the input fields. These will update when players check their answers, and can display either a checkmark (correct), an X (incorrect), or a question mark (improper formatting).

--- Assets/Demo/Scripts/FloatingPanel.cs
+++ Assets/Demo/Scripts/FloatingPanel.cs
@@ -5,64 +5,116 @@ using UnityEngine.UI;
 
 public class FloatingPanel : MonoBehaviour
 {
-    public InputField SpecificHeat;
-    public InputField MassFlowOut;
-    public InputField OutputTemp;
+    public Text QuestionPrompt;
+    public GameObject SpecificHeat, MassFlowOut, OutputTemp;
+    public Sprite CorrectPNG, IncorrectPNG, FormatPNG;
 
     DebugController debugLog;
+
+    int qDot;
+    int SpecificHeatAnswer, MassFlowOutAnswer, OutputTempAnswer;
 
     // Start is called before the first frame update
     void Start()
     {
-        debugLog = GameObject.Find("DebugController").GetComponent<DebugController>();
+        //debugLog = GameObject.Find("DebugController").GetComponent<DebugController>();
+        qDot = Random.Range(100, 2001);
+        QuestionPrompt.text += qDot.ToString() + " kW.";
+        CalculateAnswers();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //var target = Camera.main.transform.position;
-        //target.x = -target.x;
-        //target.z = -target.z;
-        //target.y = transform.position.y;
-        //transform.LookAt(target);
-        var cameraForward = Camera.current.transform.forward;
+        /*var target = Camera.main.transform.position;
+        target.x = -target.x;
+        target.z = -target.z;
+        target.y = transform.position.y;
+        transform.LookAt(target);*/
+        /*var cameraForward = Camera.current.transform.forward;
         var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
-        transform.rotation = Quaternion.LookRotation(cameraBearing);
+        transform.rotation = Quaternion.LookRotation(cameraBearing);*/
+    }
+
+    /// <summary>
+    /// This function does the math.
+    /// </summary>
+    public void CalculateAnswers()
+    {
+        SpecificHeatAnswer = qDot + 1;
+        MassFlowOutAnswer = qDot + 2;
+        OutputTempAnswer = qDot + 3;
     }
 
     public void CheckAnswers()
     {
-        if (SpecificHeat.text == "1")
+        // Checking Specific Heat
+        if (int.TryParse(SpecificHeat.GetComponentInChildren<InputField>().text, out int SpecificHeatInput))
         {
-            //Debug.Log("Specific Heat:\t\tCorrect");
-            debugLog.NewLineDebugText("Specific Heat:\t\tCorrect");
+            if (SpecificHeatInput == SpecificHeatAnswer)
+            {
+                Debug.Log("Specific Heat: Correct");
+                //debugLog.NewLineDebugText("Specific Heat: Correct");
+                SpecificHeat.GetComponentInChildren<Image>().sprite = CorrectPNG;
+            }
+            else
+            {
+                Debug.Log("Specific Heat: Incorrect");
+                //debugLog.NewLineDebugText("Specific Heat: Incorrect");
+                SpecificHeat.GetComponentInChildren<Image>().sprite = IncorrectPNG;
+            }
         }
         else
         {
-            //Debug.Log("Specific Heat:\t\tIncorrect");
-            debugLog.NewLineDebugText("Specific Heat:\tIncorrect");
+            Debug.Log("Incorrect Format");
+            //debugLog.NewLineDebugText("Incorrect Format");
+            SpecificHeat.GetComponentInChildren<Image>().sprite = FormatPNG;
         }
 
-        if (MassFlowOut.text == "1")
+        // Checking Mass Flow Rate Out
+        if (int.TryParse(MassFlowOut.GetComponentInChildren<InputField>().text, out int MassFlowOutInput))
         {
-            //Debug.Log("Mass Flow Out:\tCorrect");
-            debugLog.NewLineDebugText("Mass Flow Out:\tCorrect");
+            if (MassFlowOutInput == MassFlowOutAnswer)
+            {
+                Debug.Log("Mass Flow Out: Correct");
+                //debugLog.NewLineDebugText("Mass Flow Out: Correct");
+                MassFlowOut.GetComponentInChildren<Image>().sprite = CorrectPNG;
+            }
+            else
+            {
+                Debug.Log("Mass Flow Out: Incorrect");
+                //debugLog.NewLineDebugText("Mass Flow Out: Incorrect");
+                MassFlowOut.GetComponentInChildren<Image>().sprite = IncorrectPNG;
+            }
         }
         else
         {
-            //Debug.Log("Mass Flow Out:\tIncorrect");
-            debugLog.NewLineDebugText("Mass Flow Out:\tIncorrect");
+            Debug.Log("Incorrect Format");
+            //debugLog.NewLineDebugText("Incorrect Format");
+            MassFlowOut.GetComponentInChildren<Image>().sprite = FormatPNG;
         }
 
-        if (OutputTemp.text == "1")
+        // Checking Output Temperature
+        if (int.TryParse(OutputTemp.GetComponentInChildren<InputField>().text, out int OutputTempInput))
         {
-            //Debug.Log("Output Temperature:\tCorrect");
-            debugLog.NewLineDebugText("Output Temperature:\tCorrect");
+            if (OutputTempInput == OutputTempAnswer)
+            {
+                Debug.Log("Output Temperature: Correct");
+                //debugLog.NewLineDebugText("Output Temperature: Correct");
+                OutputTemp.GetComponentInChildren<Image>().sprite = CorrectPNG;
+            }
+            else
+            {
+                Debug.Log("Output Temperature: Incorrect");
+                //debugLog.NewLineDebugText("Output Temperature: Incorrect");
+                OutputTemp.GetComponentInChildren<Image>().sprite = IncorrectPNG;
+            }
         }
         else
         {
-            //Debug.Log("Output Temperature:\tIncorrect");
-            debugLog.NewLineDebugText("Output Temperature:\tIncorrect");
+            Debug.Log("Incorrect Format");
+            //debugLog.NewLineDebugText("Incorrect Format");
+            OutputTemp.GetComponentInChildren<Image>().sprite = FormatPNG;
         }
     }
 }
